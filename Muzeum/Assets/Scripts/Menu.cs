145b2e276514b64c6f8306c2432479c8f7d974fe
BartoszPruszkowski <bartosz.pruszkowski1996@gmail.com>
2020-04-06 17:59:12 +0200

    Dodanie menu z aktywnym przyciskiem start

---
+++ Muzeum/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void InfoButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
+
+
