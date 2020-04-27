b46e73c4de480e7829e491dcd7b7ed248827f372
ola <ola.radzik@o2.pl>
2020-04-27 14:29:55 +0200

    Pies się rusza i ma teksture

---
+++ Muzeum/Assets/MotionDog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionDog : MonoBehaviour
+{
+
+    public GameObject dog;
+
+    void Update()
+    {
+      
+        dog.transform.position += transform.up * Time.deltaTime * 0.0001f;
+
+        dog.transform.position -= transform.forward * Time.deltaTime * 0.005f;
+
+        dog.transform.position += transform.right * Time.deltaTime * 0.005f;
+  
+
+    }
+
+}
+
