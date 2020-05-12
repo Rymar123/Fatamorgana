dc901b703682c7ff9f03d45fa3b7c17e2074d1db
ola <ola.radzik@o2.pl>
2020-05-12 10:29:30 +0200

    dodanie obrazów z opisami + dzwiek ktorego nie ogarnelam

---
+++ Muzeum/Assets/ImageTargetPlayAudio.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[RequireComponent(typeof(AudioSource))]
+[System.Obsolete]
+public class ImageTargetPlayAudio : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    //public AudioSource audioSource;
+
+    AudioSource audioData;
+
+    [System.Obsolete]
+    void Start()
+    {
+        
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+        //audioSource = GameObject.Find("Audio").GetComponent<AudioSource>();
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+
+            //audioSource.Play();
+            audioData.UnPause();
+
+        }
+        else
+        {
+            // Stop audio when target is lost
+            //audioSource.Stop();
+            audioData.Pause();
+        }
+    }
+}
